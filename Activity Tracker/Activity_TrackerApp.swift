1cb388b04df2ef1b812c8dd1bed0343987d68747
joshvarg <joshva528@gmail.com>
2022-05-21 12:03:50 -0700

    Initial Commit

---
+++ Activity Tracker/Activity_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Activity_TrackerApp.swift
+//  Activity Tracker
+//
+//  Created by Joshua Varghese on 5/21/22.
+//
+
+import SwiftUI
+
+@main
+struct Activity_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
